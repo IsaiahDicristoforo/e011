8693a22660feea2a0ecfe6ab47f6df5189f54974
Bill Nicholson <nicholdw@ucmail.uc.edu>
2019-12-25 10:16:36 -0500

    Added a little more to data.txt

--- E011/E011.cs
+++ E011/E011.cs
@@ -4,6 +4,7 @@
  * 
  * Project Euler Tribute Problem I created
  * In the given file, data.txt, what is the largest prime number consisting of all prime digits? 
+ * For this problem we will allow 1 to be considered prime. We will call this the "Hannah Prime"
  */
 
 using System;
